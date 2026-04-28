457ae0d07a1844124213c8ff5871fbabf9781c77
Bright <smartme783@gmail.com>
2026-04-28 12:11:49 +0100

    updated

--- beelicious_fe/src/app/api/all-product/route.ts
+++ beelicious_fe/src/app/api/all-product/route.ts
@@ -1,5 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import Product from "../../../utility/data/allarrivals";
+import { NextRequest, NextResponse } from 'next/server';
+import Product from '../../../utility/data/allarrivals';
 
 export async function POST(req: NextRequest) {
   return NextResponse.json(Product);
